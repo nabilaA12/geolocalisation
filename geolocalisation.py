5566dcc88b63556ea08335cf522fd08e899e0701
NabilaAmm <100291125+NabilaAmm@users.noreply.github.com>
2022-03-01 12:47:07 -0500

    Update geolocalisation.py

--- geolocalisation.py
+++ geolocalisation.py
@@ -1,18 +1,20 @@
 
 from math import sqrt
 
+Pole_lat=176
+Pole_long=86
+
 #fonction permet de convertir les coordonées DMS en décimale
 def dms_en_dd(d,m,s): 
     sec=m*60+s
     dd=sec/3600+d
     return dd
 
+
  # fonction permet de calculer la distance entre deux points a(a1,b1) et b(a2,b2)
 def distance(a1,b1,a2,b2): 
     return sqrt((pow(a2-a1,2)+pow(b2-b1,2)))
 
-Pole_lat=176
-Pole_long=86
 
 ##### DMS PARIS ######
 
@@ -28,5 +30,6 @@ m1=20
 s1=56
 print ("la longitude de paris en décimale est: ",dms_en_dd(d1,m1,s1))
 
+#######
 
 print ("la distance entre paris et le pole nord est :" ,distance(Pole_lat,dms_en_dd(d,m,s),Pole_long,dms_en_dd(d1,m1,s1)))
