68db003bc020016bf7684ee25a626c9d3b8880d4
NabilaAmm <100291125+NabilaAmm@users.noreply.github.com>
2022-03-04 12:25:56 -0500

    Update geolocalisation.py

--- geolocalisation.py
+++ geolocalisation.py
@@ -13,22 +13,23 @@ def distance(b1,b2,a1,a2):
     return sqrt((pow(b2-b1,2)+pow(a2-a1,2)))
 
 
-# DMS PARIS #
-
-# En latitude
+# DMS PARIS en latitude
 d=48
 m=51
 s=12
-print ("La latitude de paris en décimale est :",dms_en_dd(d,m,s))
+paris_lat=dms_en_dd(d,m,s)
+print ("La latitude de paris en décimale est :",paris_lat)
 
-# En longitude
+# DMS PARIS en longitude
 d1=2
 m1=20
 s1=55
-print ("La longitude de paris en décimale est :",dms_en_dd(d1,m1,s1))
+paris_long=dms_en_dd(d1,m1,s1)
+print ("La longitude de paris en décimale est :",paris_long)
 
 ## Longitude et latitude du pole nord en décimale
 Pole_lat = 172
 Pole_long = 86
 
-print ("La distance entre le pole nord et paris est de :" ,distance(Pole_long,dms_en_dd(d1,m1,s1),Pole_lat,dms_en_dd(d,m,s)))
+
+print ("La distance entre le pole nord et paris est de :" ,distance(Pole_long,paris_long,Pole_lat,paris_lat))
